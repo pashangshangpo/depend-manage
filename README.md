22bad0fa67f47b4bf6248f89ba25407e36cf0d44
pashangshangpo <pashangshangpo@gmail.com>
2019-01-31 16:10:29 +0800

    chore: 完成依赖下载

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # 简介
 
 nodejs 依赖下载工具
+
+## 使用示例
+
+```cli
+download-depend --config depend.config.js
+```
+
+如果不写配置文件路径，默认为 depend.config.js
+
+depend.config.js
+```js
+module.exports = {
+  // 输出的路径
+  output: 'src/node_modules',
+  repos: [
+    {
+      // 仓库地址
+      url: 'https://github.com/pashangshangpo/node-tool.git',
+      // 是否下载依赖
+      install: false
+    },
+    'https://github.com/pashangshangpo/shell-tool.git'
+  ],
+}
+```
--- bin/download-depend.js
+++ bin/download-depend.js
@@ -1,13 +1,72 @@
 #!/usr/bin/env node
 
-Promise.resolve().then(async () => {
-  const Cli = require('commander')
-  const Path = require('path')
-  const { Git, CheckYarnInstall, Shell, Exec } = require('shell-tool')
+const Cli = require('commander')
+const { Path } = require('node-tool')
+const { Git, CheckYarnInstall, Exec } = require('shell-tool')
 
+const DownLoadDepend = async (repos, output) => {
+  let all = []
+  let checkYarn = await CheckYarnInstall()
+  let install = checkYarn ? 'yarn' : 'npm install'
+
+  for (let repo of repos) {
+    let gitPath = repo
+    let installDepend = true
+
+    if (typeof repo === 'object') {
+      gitPath = repo.url
+
+      if (repo.install === false) {
+        installDepend = false
+      }
+    }
+
+    all.push(new Promise(resolve => {
+      console.log(`正在克隆 ${gitPath}`)
+      
+      Git
+        .Clone(gitPath, output)
+        .then(res => {
+          if (res.code === 0) {
+            return res.targetPath
+          }
+          else {
+            console.error(res.error)
+
+            process.exit(1)
+          }
+        })
+        .then(targetPath => {
+          if (installDepend) {
+            console.log(`正在安装依赖 ${targetPath}`)
+          
+            Exec(`cd ${targetPath} && ${install}`).then(() => {
+              resolve()
+            })
+          }
+        })
+    }))
+  }
+
+  return Promise.all(all)
+}
+
+Promise.resolve().then(async () => {
   Cli
     .version('0.0.1')
     .description('依赖下载工具')
     .option('-c, --config [config]', '配置文件路径')
     .parse(process.argv)
+
+  if (!Cli.config) {
+    console.log('没有设置配置文件')
+    process.exit(0)
+  }
+  
+  let configPath = Path.joinApp(Cli.config)
+  let config = require(configPath)
+
+  await DownLoadDepend(config.repos, Path.joinApp(config.output))
+
+  console.log('全部依赖下载完成')
 })
